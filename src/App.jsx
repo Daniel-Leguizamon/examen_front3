bd3c64339b8663cbede9cf0a55b41fb798b438f0
DANI <dleguizamon77@gmail.com>
2023-05-30 10:45:57 -0300

    Base de examen React push 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <div className='app'>
+      <h1>Carga de Estudiantes</h1>
+      <form> </form>
+      <Card/>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+const Card = () => {
+  return (
+    <div>hola, soy una tarjeta</div>
+  )
+}
+
+export default Card
