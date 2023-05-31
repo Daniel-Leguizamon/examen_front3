58fa92e61125b83730babea8b471c7078fe2b755
DANI <dleguizamon77@gmail.com>
2023-05-31 20:44:23 -0300

    segundo push de examen parcial de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
 import Card from './components/Card'
+import { useState } from 'react';
 
 function App() {
+  
+  const [nombre,setNombre] = useState("");
+  const [formulario,setFormulario] = useState(false);
+
+  const handleSubmit = event=>{
+    event.preventDefault();
+    if (nombre==="") {
+      alert("el nombre no debe estar vacío")
+    } else {
+      alert("formulario enviado")
+    }
+    setFormulario(!formulario);
+
+    }
+  const alRealizarUnCambio = e =>{setNombre(e.target.value)}
+
+  
 
   return (
     <>
     <div className='app'>
       <h1>Carga de Estudiantes</h1>
-      <form> </form>
+      <form
+      onSubmit={handleSubmit}> 
+        <label>Nombre </label>
+        <input
+          value={nombre}
+          onChange={alRealizarUnCambio}
+        />  
+        <button type='submit'>Realizar</button>
+      </form>
       <Card/>
     </div>
 
